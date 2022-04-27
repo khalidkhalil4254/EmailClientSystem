2bf06a02e0886f4b8892f10fb46b72c7b46af6d9
khalid khalil <kalidkalil4254@gmail.com>
2022-04-27 07:15:06 +0200

    f

---
+++ src/system/event.java
@@ -0,0 +1,277 @@
+package system;
+import java.awt.event.*;
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class event extends gui{
+
+    int statusPort=1234 ,userPort=1235,passwordPort=1236,msgPort=1237,receiverPort=1238 ,newUserPort=1239,newPassPort=1240,senderPort=1241,mailSenderPort=1242,mailReceiverPort=1243,mailMsgPort=1244,mailToServerPort=1245,counter=0;
+    Socket statusSocket , usernameSocket ,passwordSocket , newUsernameSocket, newPasswordSocket , msgSocket , receiverSocket,senderSocket,mailSenderSocket,mailSocket,mailReceiverSocket;
+    DataOutputStream writeUsername , writePassword ,writeReceiver, writeMessage ,writeNewUsername,writeNewPassword;
+    DataInputStream readSignInResponse,readSignUpResponse,readReceiver,readSender,readEmail;
+    ArrayList mailBox,senders,receivers;
+    static String LogInUsername,serverAddress="192.168.1.10";
+
+    event(){
+
+
+
+        signup_btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                signin_frame.setVisible(false);
+                signup_frame.setVisible(true);
+            }
+        });
+
+
+        register_btn_signup.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //sending data to the server to store it into server database:-
+                String newUser=username_txt_signup.getText();
+                String newPass=new String(Password_txt_signup.getPassword());
+
+                username_txt_signup.setText("");
+                Password_txt_signup.setText("");
+
+                try{
+                    //sending data to the server to be handled:-
+                    newUsernameSocket=new Socket(InetAddress.getByName(serverAddress),newUserPort);
+                    writeNewUsername=new DataOutputStream(newUsernameSocket.getOutputStream());
+                    writeNewUsername.writeUTF(newUser);
+                    writeNewUsername.flush();
+                    writeNewUsername.close();
+
+
+                    newPasswordSocket=new Socket(InetAddress.getByName(serverAddress),newPassPort);
+                    writeNewPassword=new DataOutputStream(newPasswordSocket.getOutputStream());
+                    writeNewPassword.writeUTF(newPass);
+                    writeNewPassword.flush();
+                    writeNewPassword.close();
+
+
+                    //getting status from the server:-
+                    statusSocket=new Socket(InetAddress.getByName(serverAddress),statusPort);
+                    readSignUpResponse=new DataInputStream(statusSocket.getInputStream());
+                    String response= readSignUpResponse.readUTF();
+
+                    if(response.equals("Registered:yes!")){
+                        signin_frame.setVisible(true);
+                        signup_frame.setVisible(false);
+                    }else if(response.equals("Registration:failed!")){
+                        signUpStatus.setText("Register:Refused!");
+                    }
+
+
+                }catch (Exception er){
+                    signUpStatus.setText("Error Occurred!");
+                }
+            }
+        });
+
+
+        back_btn_signup.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                signin_frame.setVisible(true);
+                signup_frame.setVisible(false);
+            }
+        });
+
+
+        signin_btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    LogInUsername=username_txt.getText();
+
+                    //getting the values from the gui:-
+                    String username=username_txt.getText();
+                    String password=new String(password_txt.getPassword());
+
+                    //requesting from the server to handle it:-
+                    //opening connection with the server:-
+                    statusSocket = new Socket(InetAddress.getByName(serverAddress), statusPort);
+
+                    //sending the username:-
+                    usernameSocket=new Socket(InetAddress.getByName(serverAddress),userPort);
+                    writeUsername=new DataOutputStream(usernameSocket.getOutputStream());
+                    writeUsername.writeUTF(username);
+                    writeUsername.flush();
+                    writeUsername.close();
+
+                    //sending the password:-
+                    passwordSocket=new Socket(InetAddress.getByName(serverAddress),passwordPort);
+                    writePassword=new DataOutputStream(passwordSocket.getOutputStream());
+                    writePassword.writeUTF(password);
+                    writePassword.flush();
+                    writePassword.close();
+
+                    readSignInResponse=new DataInputStream(statusSocket.getInputStream());
+                    String response=readSignInResponse.readUTF();
+
+                    //receiving response from the server from another sender of mail:-
+                    if(response.equals("yes")){
+                        signin_frame.setVisible(false);
+                        app_frame.setVisible(true);
+                        LogInUsername=username;
+                        mailBox=new ArrayList<String>();
+
+                    }
+                    if(response.equals("no")){
+                        status.setText("Invalid Credentials!");
+                    }
+
+                }catch (Exception err){
+                    status.setText("Error!");
+                }
+
+                password_txt.setText("");
+
+            }
+        });
+
+
+
+
+        refresh_btn_NCTUMAIL.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                /////////////////////////////////////////////////synchronous updating the MailBox of the users:-//////////////////////////////////////////////////////
+                Thread clientMail=new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (true) {
+                            try {
+                                //receive emails from the server :-
+                                mailSenderSocket = new Socket(InetAddress.getByName(serverAddress), mailSenderPort);
+                                mailReceiverSocket = new Socket(InetAddress.getByName(serverAddress), mailReceiverPort);
+                                mailSocket = new Socket(InetAddress.getByName(serverAddress), mailMsgPort);
+                            } catch (Exception er) {
+                                System.out.println("er:" + er);
+                            }
+
+                            try {
+                                readSender = new DataInputStream(mailSenderSocket.getInputStream());
+                                String sender = readSender.readUTF();
+                                readSender.close();
+                                readReceiver = new DataInputStream(mailReceiverSocket.getInputStream());
+                                String receiver = readReceiver.readUTF();
+                                readReceiver.close();
+                                readEmail = new DataInputStream(mailSocket.getInputStream());
+                                String mail = readEmail.readUTF();
+                                readEmail.close();
+                                String msg = "sender:" + sender + "\n\t" + mail + "\n\n";
+
+
+                                if ((!sender.equals("") && !receiver.equals("") && !msg.equals("")) && (!sender.equals(null) && !receiver.equals(null) && !msg.equals(null))) {
+                                    if (LogInUsername.equals(receiver)) {
+                                        if (!mailBox.contains(msg)) {
+                                            mailBox.add(msg);
+                                            Emails_txt.setText(Arrays.toString(mailBox.toArray()));
+                                        }
+                                    }
+                                }
+
+                            } catch (Exception err) {
+                                System.out.println("Error: receiving mail!!!" + err);
+                            }
+                        }
+
+                    }
+                });
+                //starting mail for client service(thread):-
+                clientMail.start();
+            }
+        });
+
+
+
+
+        signOut_btn_NCTUMAIL.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                app_frame.setVisible(false);
+                signin_frame.setVisible(true);
+                Emails_txt.setText("");
+            }
+        });
+
+
+        send_btn_NCTUMAIL.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                app_frame.setVisible(false);
+                sendEmail_frame.setVisible(true);
+                sender_txt_sendEmail.setText(LogInUsername);
+            }
+        });
+
+        back_btn_sendEmail.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                app_frame.setVisible(true);
+                sendEmail_frame.setVisible(false);
+            }
+        });
+
+
+
+
+        send_btn_sendEmail.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                try {
+
+                    if(!receiver_txt_sendEmail.getText().equals("") && !mail_txt_sendEmail.getText().equals("")) {
+                        //sending the sender name and receiver name to the server:-
+                        senderSocket = new Socket(InetAddress.getByName(serverAddress), senderPort);
+                        writeUsername = new DataOutputStream(senderSocket.getOutputStream());
+                        writeUsername.writeUTF(LogInUsername);
+                        writeUsername.flush();
+                        writeUsername.close();
+                        senderSocket.close();
+
+
+                        receiverSocket = new Socket(InetAddress.getByName(serverAddress), receiverPort);
+                        writeReceiver = new DataOutputStream(receiverSocket.getOutputStream());
+                        writeReceiver.writeUTF(receiver_txt_sendEmail.getText());
+                        writeReceiver.flush();
+                        writeReceiver.close();
+                        receiverSocket.close();
+
+
+                        msgSocket = new Socket(InetAddress.getByName(serverAddress), mailToServerPort);
+                        writeMessage = new DataOutputStream(msgSocket.getOutputStream());
+                        writeMessage.writeUTF(mail_txt_sendEmail.getText());
+                        writeMessage.flush();
+                        writeMessage.close();
+                        msgSocket.close();
+
+                        sender_txt_sendEmail.setText("");
+                        receiver_txt_sendEmail.setText("");
+                        mail_txt_sendEmail.setText("");
+
+                        sendEmail_frame.setVisible(false);
+                        app_frame.setVisible(true);
+
+                        System.out.println("mail sent successfully!");
+
+                        counter++;
+                    }
+
+                }catch (Exception er){
+                    System.out.println("send_btn_sendEmail error:"+er);
+                }
+
+            }
+        });
+
+
+
+    }
+}
---
+++ src/system/gui.java
@@ -0,0 +1,194 @@
+package system;
+import javax.swing.*;
+
+public class gui {
+
+
+    JFrame signin_frame , signup_frame, app_frame , sendEmail_frame;
+    JLabel username_lbl , password_lbl , status ,username_lbl_signup,Password_lbl_signup ,signUpStatus ,mailBox_lbl,sender_lbl_sendEmail,receiver_lbl_sendEmail,mail_lbl_sendEmail;
+    JTextField username_txt  ,username_txt_signup , sender_txt_sendEmail, receiver_txt_sendEmail;
+    JPasswordField password_txt,Password_txt_signup ;
+    JButton signin_btn , signup_btn ,back_btn_signup , register_btn_signup , signOut_btn_NCTUMAIL , send_btn_NCTUMAIL,refresh_btn_NCTUMAIL , send_btn_sendEmail ,back_btn_sendEmail;
+    JTextArea Emails_txt,mail_txt_sendEmail;
+    JScrollPane scroll,scroll1;
+
+
+    gui(){
+
+
+        //creating the JFrames of the app:-
+        signin_frame=new JFrame("SignIn");
+        signup_frame=new JFrame("SignUp");
+        app_frame=new JFrame("NCTU-MAIL System");
+        sendEmail_frame=new JFrame("SEND EMAIL");
+        sendEmail_frame.setResizable(false);
+        sendEmail_frame.setSize(500,500);
+        sendEmail_frame.setLayout(null);
+        sendEmail_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        signin_frame.setResizable(false);
+        signin_frame.setSize(500,235);
+        signin_frame.setLayout(null);
+        signin_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        signup_frame.setResizable(false);
+        signup_frame.setSize(480,360);
+        signup_frame.setLayout(null);
+        signup_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app_frame.setResizable(false);
+        app_frame.setSize(1024,678);
+        app_frame.setLayout(null);
+        app_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+        //creating the components of the signIn JFrame:-
+        status=new JLabel("SignIn");
+        status.setBounds(220,100,50,20);
+
+        username_lbl=new JLabel("Username:");
+        username_lbl.setBounds(90,20,80,20);
+
+        password_lbl=new JLabel("Password:");
+        password_lbl.setBounds(90,60,80,20);
+
+        username_txt=new JTextField();
+        username_txt.setBounds(170,20,210,30);
+
+        password_txt=new JPasswordField();
+        password_txt.setBounds(170,60,210,30);
+
+        signin_btn=new JButton("SignIn");
+        signin_btn.setBounds(300,120,80,35);
+
+        signup_btn=new JButton("SignUp");
+        signup_btn.setBounds(90,120,80,35);
+
+
+        //adding the components of the signIn JFrame:-
+        signin_frame.add(username_lbl);
+        signin_frame.add(password_lbl);
+        signin_frame.add(username_txt);
+        signin_frame.add(password_txt);
+        signin_frame.add(signin_btn);
+        signin_frame.add(signup_btn);
+        signin_frame.add(status);
+
+
+
+
+
+
+
+
+
+        //creating the components of the signUp JFrame:-
+        username_lbl_signup=new JLabel("New Username:");
+        username_lbl_signup.setBounds(40,50,120,30);
+
+        username_txt_signup=new JTextField();
+        username_txt_signup.setBounds(160,50,220,30);
+
+        Password_lbl_signup=new JLabel("Create Password:");
+        Password_lbl_signup.setBounds(40,120,120,30);
+
+        Password_txt_signup=new JPasswordField();
+        Password_txt_signup.setBounds(160,120,220,30);
+
+        back_btn_signup=new JButton("Back");
+        back_btn_signup.setBounds(40,200,80,50);
+
+        register_btn_signup=new JButton("Register");
+        register_btn_signup.setBounds(280,200,100,50);
+
+        signUpStatus=new JLabel("SignUp");
+        signUpStatus.setBounds(158,180,80,20);
+
+        //adding the components of the signUp JFrame:-
+        signup_frame.add(username_lbl_signup);
+        signup_frame.add(Password_lbl_signup);
+        signup_frame.add(back_btn_signup);
+        signup_frame.add(username_txt_signup);
+        signup_frame.add(Password_txt_signup);
+        signup_frame.add(register_btn_signup);
+        signup_frame.add(signUpStatus);
+
+
+
+        //creating the components of the app JFrame:-
+        mailBox_lbl=new JLabel("Your MailBox :-");
+        mailBox_lbl.setBounds(35,35,120,20);
+
+        Emails_txt=new JTextArea();
+        Emails_txt.setToolTipText("MailBox");
+        Emails_txt.setEditable(false);
+        scroll = new JScrollPane (Emails_txt, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scroll.setBounds(35,80,935,420);
+
+        signOut_btn_NCTUMAIL=new JButton("SignOut");
+        signOut_btn_NCTUMAIL.setBounds(35,550,80,50);
+
+        refresh_btn_NCTUMAIL=new JButton("Refresh");
+        refresh_btn_NCTUMAIL.setBounds(442,550,80,50);
+
+        send_btn_NCTUMAIL=new JButton("Send Email");
+        send_btn_NCTUMAIL.setBounds(850,550,120,50);
+
+
+        //adding the components of the app JFrame:-
+        app_frame.add(mailBox_lbl);
+        app_frame.add(scroll);
+        app_frame.add(signOut_btn_NCTUMAIL);
+        app_frame.add(send_btn_NCTUMAIL);
+        app_frame.add(refresh_btn_NCTUMAIL);
+
+
+
+        //creating the components of the sendEmail JFrame:-
+        sender_lbl_sendEmail=new JLabel("TO :");
+        sender_lbl_sendEmail.setBounds(35,80,50,20);
+
+        sender_txt_sendEmail=new JTextField();
+        sender_txt_sendEmail.setEditable(false);
+        sender_txt_sendEmail.setToolTipText("sender");
+        sender_txt_sendEmail.setBounds(100,40,240,30);
+
+        receiver_lbl_sendEmail=new JLabel("From :");
+        receiver_lbl_sendEmail.setBounds(35,40,50,20);
+
+        receiver_txt_sendEmail=new JTextField();
+        receiver_txt_sendEmail.setToolTipText("receiver");
+        receiver_txt_sendEmail.setBounds(100,80,240,30);
+
+        mail_lbl_sendEmail=new JLabel("Mail:");
+        mail_lbl_sendEmail.setBounds(35,140,100,20);
+
+
+        mail_txt_sendEmail=new JTextArea();
+        mail_txt_sendEmail.setToolTipText("MailBox");
+        scroll1 = new JScrollPane (mail_txt_sendEmail, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scroll1.setBounds(100,140,240,200);
+
+
+        send_btn_sendEmail=new JButton("SEND");
+        send_btn_sendEmail.setBounds(365,380,80,50);
+
+        back_btn_sendEmail=new JButton("BACK");
+        back_btn_sendEmail.setBounds(35,380,80,50);
+
+
+        //adding the components of the sendEmail JFrame:-
+        sendEmail_frame.add(sender_lbl_sendEmail);
+        sendEmail_frame.add(sender_txt_sendEmail);
+        sendEmail_frame.add(receiver_lbl_sendEmail);
+        sendEmail_frame.add(receiver_txt_sendEmail);
+        sendEmail_frame.add(send_btn_sendEmail);
+        sendEmail_frame.add(back_btn_sendEmail);
+        sendEmail_frame.add(mail_lbl_sendEmail);
+        sendEmail_frame.add(scroll1);
+
+
+
+
+        signin_frame.setVisible(true);
+
+    }
+}
